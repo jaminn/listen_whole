4fcd6c296d54b685f741b7349863c351ea7082cc
jaminn <rnnwkals1@naver.com>
2016-08-23 10:27:23 +0300

    uploaded

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var cookie = require('cookie');
 var session = require('express-session');
 var sessionstore = require('sessionstore');
 var store = sessionstore.createSessionStore();
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var exam = require('./routes/exam');
@@ -26,70 +25,126 @@ var media = require('./routes/media');
 var media_ex = require('./routes/media_ex');
 var profile = require('./routes/profile');
 var upload = require('./routes/upload');
-
 var app = express();
-
 var mongoose = require('mongoose');
-mongoose.connect(' mongodb://rnnwkals1:hi000319@ds029655.mlab.com:29655/listendb');
+mongoose.connect('mongodb://rnnwkals1:hi000319@ds029655.mlab.com:29655/listendb');
 var db = mongoose.connection;
-
 var UserSchema = new mongoose.Schema({
-        email: { type : String, required:true, unique:true},
-        passwd: { type : String, required:true},
-        nickname: { type : String, required:true, unique:true},
-        isLogined: {type: Boolean, required:true, default: false},
-        Level: {type: Number, default: 0},
-        score: {type: Number, default: 0},
-        exp: {type: Number, default: 0},
-
-        PlayedVid: [{
-          Vid : {type: String},
-          cnt : {type: Number, default: 0},
-          word : [{type: String}],
-          HowmanyWrong : [{type: Number}],
-          HowmanyPlay: { type: Number},
-          playtime : {type: Number, default: 0},
-          playtimeAg : {type: Number, default: 0}
-        }],
-
-        HowMany_wrongs: {type: String}
+    email: {
+        type: String
+        , required: true
+        , unique: true
+    }
+    , passwd: {
+        type: String
+        , required: true
+    }
+    , nickname: {
+        type: String
+        , required: true
+        , unique: true
+    }
+    , isLogined: {
+        type: Boolean
+        , required: true
+        , default: false
+    }
+    , Level: {
+        type: Number
+        , default: 0
+    }
+    , score: {
+        type: Number
+        , default: 0
+    }
+    , exp: {
+        type: Number
+        , default: 0
+    }
+    , PlayedVid: [{
+        Vid: {
+            type: String
+        }
+        , cnt: {
+            type: Number
+            , default: 0
+        }
+        , word: [{
+            type: String
+        }]
+        , HowmanyWrong: [{
+            type: Number
+        }]
+        , HowmanyPlay: {
+            type: Number
+        }
+        , playtime: {
+            type: Number
+            , default: 0
+        }
+        , playtimeAg: {
+            type: Number
+            , default: 0
+        }
+        }]
+    , HowMany_wrongs: {
+        type: String
+    }
 });
-
-Users = mongoose.model('users',UserSchema);
-
+Users = mongoose.model('users', UserSchema);
 var VidSchema = new mongoose.Schema({
-        name: {type: String, required:true, unique:true},
-        vid: {type: String, required:true, unique:true},
-        tag: {type: String, required: true},
-        intro: {type: String, required: true}
+    name: {
+        type: String
+        , required: true
+        , unique: true
+    }
+    , vid: {
+        type: String
+        , required: true
+        , unique: true
+    }
+    , tag: {
+        type: String
+        , required: true
+    }
+    , intro: {
+        type: String
+        , required: true
+    }
 });
-
 Vids = mongoose.model('vids', VidSchema);
-
 var roomSchema = new mongoose.Schema({
-          room: {type: String},
-          username: {type: String},
-          vid: {type: String},
-});
-
+    room: {
+        type: String
+    }
+    , username: {
+        type: String
+    }
+    , vid: {
+        type: String
+    }
+, });
 Rooms = mongoose.model('room', roomSchema);
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use( session( { store: store, secret: '앙기모띠', saveUninitialized: true}));
-
-
+app.use(express.static('sub'));
+app.use(session({
+    store: store
+    , secret: '앙기모띠'
+    , saveUninitialized: true
+    , resave: true
+    , saveUninitialized: true
+}));
 //url
 app.use('/', routes);
 app.use('/exam', exam);
@@ -107,37 +162,31 @@ app.use('/media', media);
 app.use('/media_ex', media_ex);
 app.use('/profile', profile);
 app.use('/upload', upload);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message
+            , error: err
+        });
     });
-  });
 }
-
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message
+        , error: {}
+    });
 });
-
-
 module.exports = app;
--- public/javascripts/asdf_main.js
+++ public/javascripts/asdf_main.js
@@ -3,11 +3,12 @@
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         var srtF = ""; //전문(원문) 텍스트 파일
-        var parsed_srt; //원문 파싱된
+        var parsed_srt = []; //원문 파싱된
         var quiz_counter = 0; //카운터: 몇번째 시간대 인지
         var unshuffled; //셔플 안된 단어들
         var howMany_del = 0; //몇번이나 지웠나?
         var allRightBool = false; //전부 맞쳤을 경우
+        var replayBool = false;
         var playByButton = false; //스타트 버튼으로 재생했는가
         var HowMany_wrongs = 0; //몇번이나 틀렸나?
         var wrongsCnt = 0;
@@ -16,11 +17,29 @@
         var vidID; //비디오 아이디
         var SuffButton = ["apple", "banna", "orange", "kiwi"]; //화면에 표시되는값
         var srtLast = 0; //srt파싱된것의 마지막 번호
+        var stop4quiz = false;
         var player;
         $(document).ready(function () {
             console.log("page ready!");
-            document.getElementById('player').style.pointerEvents = 'none'; //실사용시 add this line
+            //document.getElementById('player').style.pointerEvents = 'none'; //실사용시 add this line
         });
+
+        function HtmlDecode(s) {
+            var el = document.createElement("div");
+            el.innerHTML = s;
+            result1 = el.innerText || el.textContent;
+            var el2 = document.createElement("div");
+            el2.innerHTML = result1;
+            result2 = el2.innerText || el2.textContent;
+            return result2;
+        }
+
+        function wordOnly(s) {
+            var sen = s;
+            sen = sen.replace(/\B[_+.,!@#$%^&*();\\/|<>"']+\b/g, "").toLowerCase();
+            sen = sen.replace(/\b[_+.,!@#$%^&*();\\/|<>"']+\B/g, "").toLowerCase();
+            return sen;
+        }
 
         function getParams() {
             var param = new Array();
@@ -45,10 +64,31 @@
             time++;
         }
 
+        function getSubTry(data, event) {
+            var mytext;
+            var mystart;
+            var myend;
+            [].forEach.call(data.getElementsByTagName('text'), function (val) {
+                mytext = HtmlDecode(val.innerHTML);
+                mystart = val.getAttribute("start") * 1000;
+                myend = mystart + val.getAttribute("dur") * 1000;
+                parsed_srt.push({
+                    text: mytext
+                    , startTime: mystart
+                    , endTime: myend
+                });
+            });
+            srtLast = parsed_srt.length - 1;
+            event.target.cueVideoById({
+                videoId: p['vid']
+            });
+            $("#starter").removeClass("myHide");
+        }
+
         function onYouTubeIframeAPIReady() {
             player = new YT.Player('player', {
                 playerVars: {
-                    controls: 0 //개발시에는 controls: 2, 실사용시에는 controls: 0
+                    controls: 2 //개발시에는 controls: 2, 실사용시에는 controls: 0 
                         
                     , cc_load_policy: 1
                     , rel: 0
@@ -68,7 +108,8 @@
         }
 
         function onPlayerReady(event) {
-            $.get("/sub/" + p["vid"] + ".srt", function (data) {
+            var request = $.get("static/sub/" + p["vid"] + ".srt");
+            request.success(function (data) {
                 srtF = data;
                 parsed_srt = from_srt(srtF, true);
                 srtLast = parsed_srt.length - 1;
@@ -77,6 +118,41 @@
                     videoId: p['vid']
                 });
                 $("#starter").removeClass("myHide");
+            });
+            request.fail(function () {
+                console.log("자막을 가져오지 못함(자기 컴)");
+                $.ajax({
+                    url: 'http://video.google.com/timedtext?lang=en&v=' + p['vid']
+                    , success: function (data) {
+                        try {
+                            getSubTry(data, event);
+                        }
+                        catch (e) {
+                            $.ajax({
+                                url: 'http://video.google.com/timedtext?lang=en-GB&v=' + p['vid']
+                                , success: function (data) {
+                                    try {
+                                        getSubTry(data, event);
+                                    }
+                                    catch (e) {
+                                        console.log(e);
+                                        console.log("자막을 가져오지 못함(유튜브)");
+                                        alert("자막이 없습니다.");
+                                        window.location.href = "/";
+                                    }
+                                }
+                                , error: function () {
+                                    console.log("자막을 가져오지 못함(유튜브)");
+                                    alert("로딩error");
+                                }
+                            });
+                        }
+                    }
+                    , error: function () {
+                        console.log("자막을 가져오지 못함(유튜브)");
+                        alert("로딩error");
+                    }
+                });
             });
         }
 
@@ -87,7 +163,8 @@
                 break;
             case YT.PlayerState.ENDED:
                 console.log('ended');
-                $('#myModal3').modal('show')
+                alert("END");
+                window.location.href = "/";
                 break;
             case YT.PlayerState.PLAYING:
                 if (!playByButton) {
@@ -95,6 +172,10 @@
                     alert("start버튼으로 재생해주세요");
                 }
                 console.log('--playing--');
+                stop4quiz = false;
+                setTimeout(function () {
+                    replayBool = false;
+                }, 3000);
                 my_timer = setInterval(check_where, 5);
                 $("#buttonP").html(""); //버튼 초기화
                 //$("#textP").html("");  //문장 초기화
@@ -121,24 +202,27 @@
             player.playVideo();
             var fn = function () {
                 player.playVideo();
-                $(document).on("keydown", keyCtrl);
             }
             setTimeout(fn, 3000);
             $("#starter").remove();
             playByButton = true;
-            my_timer = setInterval(check_where, 5);
+            my_timer = setInterval(check_where, 10);
             progress_timer = setInterval(function () {
                 var playerTotalTime = player.getDuration();
                 var playerCurrentTime = Math.round(player.getCurrentTime());
                 var playerTimeDifference = (playerCurrentTime / playerTotalTime) * 100;
-                var playerTimePercent = Math.round(playerTimeDifference);
-                console.log(playerTimePercent + '%');
+                var playerTimePercent = playerTimeDifference.toFixed(2);
+                //console.log(playerTimePercent + '%');
                 $(".progress-bar").width(playerTimePercent + '%');
-            }, 200);
+            }, 100);
         }
 
         function selected_word(i) {
             return $("#buttonP").children().eq(i).children().eq(0).get(0);
+        }
+
+        function id2number(id) {
+            return parseInt(id.substr(1));
         }
         $(document).keydown(function (e) {
             switch (e.keyCode) {
@@ -166,6 +250,7 @@
                 console.log("you pressed 37(<-) left key");
                 if (quiz_counter !== 0) {
                     clearInterval(my_timer);
+                    replayBool = false;
                     player.seekTo(getPastStart());
                     prvWhere();
                     player.playVideo();
@@ -175,8 +260,17 @@
                 console.log("you pressed 39(->) right key");
                 if (quiz_counter < srtLast) {
                     clearInterval(my_timer);
-                    player.seekTo(getFutureStart());
-                    nxtWhere();
+                    replayBool = false;
+                    if (!stop4quiz) {
+                        if (parsed_srt[0].startTime / 1000 > player.getCurrentTime().toFixed(2)) {
+                            player.seekTo(getNowStart());
+                            nxtWhere();
+                        }
+                        else {
+                            player.seekTo(getFutureStart());
+                            nxtWhere();
+                        }
+                    }
                     player.playVideo();
                 }
                 break;
@@ -187,7 +281,9 @@
             words = words.replace(/<i>/g, "");
             words = words.replace(/<\/i>/g, "");
             words = words.replace(/\n/g, " ");
-            //words=words.replace(/-/g," ");
+            words = words.replace(/--/g, "");
+            words = words.replace(/\([\s\S]*?\)/g, "");
+            words = words.replace(/\B[^\w' ]+\B/g, "");
             words = words.replace(/ +/g, " ");
             words = words.replace(/^\s*/, '');
             words = words.replace(/\s*$/, '');
@@ -215,7 +311,7 @@
                 return parsed_srt[quiz_counter].endTime / 1000;
             }
             else {
-                return player.durationMs - 1000;
+                return player.getDuration() - 0.5;
             }
         }
 
@@ -224,25 +320,25 @@
                 return parsed_srt[quiz_counter].startTime / 1000;
             }
             else {
-                return player.durationMs - 1000;
+                return player.getDuration() - 0.5;
             }
         }
 
         function getFuture() {
-            if (quiz_counter < srtLast) {
+            if (quiz_counter <= srtLast) {
                 return parsed_srt[quiz_counter + 1].endTime / 1000;
             }
             else {
-                return player.durationMs - 1000;
+                return player.getDuration() - 0.5;
             }
         }
 
         function getFutureStart() {
-            if (quiz_counter < srtLast) {
+            if (quiz_counter <= srtLast) {
                 return parsed_srt[quiz_counter + 1].startTime / 1000;
             }
             else {
-                return player.durationMs - 1000;
+                return player.getDuration() - 0.5;
             }
         }
 
@@ -251,8 +347,10 @@
             if (!allRightBool) {
                 if (getPast() > How_video_pasted) { //진행시간이 과거 보다 작으면
                     if (quiz_counter > 0) {
-                        console.log(quiz_counter + ": ViewPrv");
-                        prvWhere(); //카운터를 줄인다.
+                        if (!replayBool && !allRightBool) {
+                            console.log(quiz_counter + ": ViewPrv");
+                            prvWhere(); //카운터를 줄인다.
+                        }
                     }
                 }
                 if (getFuture() < How_video_pasted) { //진행시간이 미래 보다 크면
@@ -262,9 +360,11 @@
                     }
                 }
                 else {
-                    if (getNow() <= How_video_pasted) { //진행시간이 미래 보다 작고, 현재보다 크면
-                        $("#buttonP").html("");
+                    if (getNow() <= How_video_pasted && ("" != wordOnly(replaces(parsed_srt[quiz_counter].text)))) { //진행시간이 미래 보다 작고, 현재보다 크면
                         console.log("진행 현재보다 큼(멈춤)");
+                        replayBool = false;
+                        stop4quiz = true;
+                        $("#buttonP").html("");
                         player.pauseVideo();
                         unshuffled = parsed_srt[quiz_counter].text;
                         unshuffled = replaces(unshuffled);
@@ -274,9 +374,9 @@
                         });
                         SuffButton = shuffle(SuffButton);
                         [0, 1, 2, 3].forEach(function (my_x) {
-                            $("#buttonP").append('<div style="float:left;margin-left:15px"><button class="btn btn-primary btn-lg bu" onclick="my_dis(this)">' + SuffButton[my_x] + "</button></div>");
+                            $("#buttonP").append('<div class="textboxWidth" style=" float: left;"><button id=b' + my_x + ' class="btn btn-primary btn-lg center-block" style="color:#000;font-size:17px;position: relative; display: block; height:80px;width:85%; padding:0; margin:0 auto;" onclick="my_dis(this)">' + wordOnly(SuffButton[my_x]) + "</button></div>");
                         });
-                        $("#ad").html("");
+                        $("#textP").html("");
                         nxtWhere();
                         console.log(quiz_counter + ": 새 버튼 생성");
                     }
@@ -285,21 +385,22 @@
             else if (getPast() <= How_video_pasted) {
                 allRightBool = false;
                 if (getPastStart() <= How_video_pasted) { //진행시간이 미래 보다 작고, 현재보다 크면
-                    $("ad").html(""); // 자막 지우기
+                    $("#textP").html(""); // 자막 지우기
                 }
             }
         }
 
         function replay() {
-            prvWhere();
             console.log(quiz_counter + ":replay가 불려짐");
-            player.seekTo(getNowStart());
+            prvWhere();
+            player.seekTo(getNowStart() - 0.1);
+            replayBool = true;
             player.playVideo();
             HowMany_wrongs = 0;
         }
 
         function my_dis(my) {
-            if (unshuffled[howMany_del] == my.innerHTML) { //정답일 경우
+            if (wordOnly(unshuffled[howMany_del]) == wordOnly(SuffButton[id2number(my.id)])) { //정답일 경우[
                 $("#buttonP").html("");
                 $("#hintP").html("");
                 [0, 1, 2, 3].forEach(function (i) {
@@ -307,13 +408,17 @@
                 });
                 SuffButton = shuffle(SuffButton);
                 [0, 1, 2, 3].forEach(function (my_x) {
-                    $("#buttonP").append('<div style="float:left;margin-left:15px"><button class="btn btn-primary btn-lg bu" onclick="my_dis(this)">' + SuffButton[my_x] + "</button></div>");
+                    $("#buttonP").append('<div class="textboxWidth" style="float: left;"><button id=b' + my_x + ' class="btn btn-primary btn-lg center-block" style="color:#000;font-size:17px;position: relative; display: block; height:80px;width: 85%;padding: 0; margin:0 auto;" onclick="my_dis(this)">' + wordOnly(SuffButton[my_x]) + "</button></div>");
                 });
                 //$("#textP").append(unshuffled[howMany_del]);
-                $("#ad").append(unshuffled[howMany_del] + " ");
+                $("#textP").append(unshuffled[howMany_del] + " ");
                 howMany_del++;
                 if (unshuffled.length == howMany_del) { //전부 맞첬을 경우
-                    player.seekTo(getPastStart());
+                    $("#myprogress").addClass("blinkGreen");
+                    setTimeout(function () {
+                        $("#myprogress").removeClass("blinkGreen");
+                    }, 1000);
+                    player.seekTo(getPastStart() - 0.1);
                     player.playVideo();
                     allRightBool = true;
                     howMany_del = 0;
@@ -322,15 +427,19 @@
             else { //틀릴 경우
                 HowMany_wrongs++;
                 wrongsCnt++;
-                wrongWord += my.innerHTML + ",";
+                wrongWord += SuffButton[id2number(my.id)] + ",";
                 if (HowMany_wrongs == 3) { //3번 틀리면 다시 시작.
                     console.log("3번 틀려서 replay를 부름");
+                    $("#myprogress").addClass("blinkRed");
+                    setTimeout(function () {
+                        $("#myprogress").removeClass("blinkRed");
+                    }, 1000);
                     replay();
                 }
                 else {
                     [0, 1, 2, 3].forEach(function (i) {
-                        if (unshuffled[howMany_del] == selected_word(i).innerHTML) {
-                            selected_word(i).style.fontWeight = "bold";
+                        if (wordOnly(unshuffled[howMany_del]) == wordOnly(SuffButton[id2number(selected_word(i).id)])) {
+                            selected_word(i).style.color = "dodgerblue";
                         }
                     });
                     $("#hintP").html("<h1>" + unshuffled[howMany_del] + "</h1>");
